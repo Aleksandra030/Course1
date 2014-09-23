5e9522546ded57c5742a3196deec5eabcd2fd523
ANA <ANA@ANA-PC>
2014-09-23 16:28:54 +0200

    new commit

--- web/js/main.js
+++ web/js/main.js
@@ -64,15 +64,14 @@ function showProjects(data) {
         if(c.typicalAgeRange){
             age = c.typicalAgeRange;
         }
-         var link = "";
+         var link = "http://localhost:8080/MyFirstREST/target='_blank'";
         if(c.license){
             link = c.license;
         }
         $('#searchResultDiv').append("<div class=\"coursesDiv\">\n\
-<table class=\"projectTable\"><tr><td colspan=\"2\"> <h3><a href="+c.provider.url+" target='_blank'>"+c.provider.url+"</a></h3></td></tr>\n\
-<tr><td width=\"60%\" align=left><h1>"+c.name+"</h1></td><td>\n\
+<table class=\"projectTable\"><tr><td colspan=\"2\"> <h3>Provider: <a href="+c.provider.url+" target='_blank'>"+c.provider.name+"</a></h3></td></tr>\n\
+<tr><td width=\"60%\" align=left><h1><a href="+link+" target='_blank'>"+c.name+"</a></h1></td><td>\n\
 TypicalAgeRange :<h3>"+age+"</h3></td><td>Language: <h3>"+lang+"</h3></td><td>\n\</tr>\n\
-<tr><td colspan=\"2\"> <h3><a href="+link+" target='_blank'>Web site</a></h3></td></tr>\n\
 <tr ><td colspan=\"2\"><h3 >Description:</h3>"+c.description+"</td></tr>\n\n\
 <tr class="+j+">");
 if(c.duration){
